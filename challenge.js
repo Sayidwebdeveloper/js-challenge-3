a5ae2299812607d77da5124bf73ddb6e2f3632cf
Sayidwebdeveloper <sayidfuad4027@gmail.com>
2022-12-26 22:54:54 +0300

    my second activity

--- challenge.js
+++ challenge.js
@@ -1,2 +1,7 @@
 // Good Luck! You got this 💪🏾
-// Write your code here.
+// Write your code here. 
+let bill =200
+let tip = 20
+console.log(tip + bill);
+
+//  Ustaad maan joogin axdii so, sidaa ayaan u fahmay exerciseka sorry hadaan baal maray
